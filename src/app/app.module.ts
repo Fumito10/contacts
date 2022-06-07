ecf6a5f64b9926de69c86c7f06fae9c1f39a344e
Fumito10 <ialucard350@gmail.com>
2022-06-06 20:34:08 -0500

    logic and structure project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -6,11 +7,22 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { firebaseConfig } from 'src/environments/firebase-config';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore())
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/environments/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCC42CwM57iwNf88KhYAfx7vKDcatAC0to",
+    authDomain: "contacto-app-599d3.firebaseapp.com",
+    projectId: "contacto-app-599d3",
+    storageBucket: "contacto-app-599d3.appspot.com",
+    messagingSenderId: "696909421679",
+    appId: "1:696909421679:web:bbd25d3632397c865e019a",
+    measurementId: "G-QZ6WRZQ9L3"
+  };
